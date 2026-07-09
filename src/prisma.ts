02bfcfe31e49064c359d80c7e68d17905510e1fa
Pasindu Pramod Weerarathne <pasindu@surreytechconsulting.lk>
2026-07-09 14:51:22 +0530

    change parent folder

---
+++ src/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "./generated/prisma/client";
+
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
